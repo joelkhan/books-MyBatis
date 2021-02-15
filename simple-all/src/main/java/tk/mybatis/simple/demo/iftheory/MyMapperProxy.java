5f8360f93f6854b094cc2dfe03eced395e0b1a49
han_kai <han_kai@uxsino.com>
2021-02-15 10:10:30 +0800

    readming some new...

---
+++ simple-all/src/main/java/tk/mybatis/simple/demo/iftheory/MyMapperProxy.java
@@ -0,0 +1,34 @@
+package tk.mybatis.simple.demo.iftheory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+/*
+ * 2.8小节的示例代码
+ * */
+public class MyMapperProxy<T> implements InvocationHandler {
+  private Class<T> mapperinterface;
+  private SqlSession sqlSession;
+  
+  public MyMapperProxy(Class<T> mapperinterface, SqlSession sqlSession ) {
+    this.mapperinterface = mapperinterface;
+    this.sqlSession = sqlSession;
+  }
+  
+  @Override
+  public Object invoke(Object proxy , Method method, Object[] args) throws Throwable {
+    System.out.println("namespace + methodId = " + (mapperinterface.getCanonicalName() + "." + 
+        method.getName()));
+    // 针对不同的 sql 类型，需要调用 sqlSession 不同的方法
+    // 接口方法中的参数也有很多情况 ，这里只考虑没有参数的情况
+    List<T> list= sqlSession.selectList(mapperinterface.getCanonicalName() + "." + 
+        method.getName());
+    // 返回值也有很多情况，这里不做处理直接返回
+    return list;
+  }
+  
+}
+
---
+++ simple-all/src/test/java/tk/mybatis/simple/demo/iftheory/MyMapperProxyTest.java
@@ -0,0 +1,30 @@
+package tk.mybatis.simple.demo.iftheory;
+
+import java.lang.reflect.Proxy;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import tk.mybatis.simple.mapper.BaseMapperTest;
+import tk.mybatis.simple.mapper.UserMapper;
+import tk.mybatis.simple.model.SysUser;
+
+/*
+ * 2.8小节的示例代码
+ * */
+public class MyMapperProxyTest extends BaseMapperTest {
+
+  @Test
+  public void testDemoIftheory() {
+    SqlSession sqlSession = getSqlSession();
+
+    MyMapperProxy userMapperProxy = new MyMapperProxy(UserMapper.class, sqlSession);
+    UserMapper userMapper = (UserMapper) Proxy.newProxyInstance(
+        Thread.currentThread().getContextClassLoader(),
+        new Class[] { UserMapper.class }, 
+        userMapperProxy);
+    // 调用 selectAll 方法
+    List<SysUser> user = userMapper.selectAll();
+  }
+
+}
+
